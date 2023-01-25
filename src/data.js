062f0e86d95f1d0ad4efbf9ca5744150c9825842
Junaid Malik <junmalik109@gmail.com>
2023-01-25 19:48:58 +0000

    boilerplate config

---
+++ src/data.js
@@ -0,0 +1,45 @@
+// import slider images
+import Avatar1 from '../src/assets/img/avt1.png';
+import Avatar2 from '../src/assets/img/avt2.png';
+import Avatar3 from '../src/assets/img/avt3.png';
+
+export const slider = [
+  {
+    message:
+      'brand bleeding-edge supply-chains, whereas proactive e-services. Quickly incentivize sticky e-business after sticky e-business.',
+    image: Avatar1,
+    name: 'Jacky Thierry',
+    email: 'Jacky.Thierry@gmail.com',
+  },
+  {
+    message: 'brand bleeding-edge supply-chains, whereas proactive e-services.',
+    image: Avatar2,
+    name: 'Jacky Thierry',
+    email: ' ',
+  },
+  {
+    message: 'brand bleeding-edge supply-chains, whereas proactive e-services.',
+    image: Avatar3,
+    name: 'Jacky Thierry',
+    email: ' ',
+  },
+  {
+    message: 'brand bleeding-edge supply-chains, whereas proactive e-services.',
+    image: Avatar3,
+    name: 'Jacky Thierry',
+    email: ' ',
+  },
+  {
+    message: 'brand bleeding-edge supply-chains, whereas proactive e-services.',
+    image: Avatar3,
+    name: 'Jacky Thierry',
+    email: ' ',
+  },
+  {
+    message: 'brand bleeding-edge supply-chains, whereas proactive e-services.',
+    image: Avatar3,
+    name: 'Jacky Thierry',
+    email: ' ',
+  },
+
+];
