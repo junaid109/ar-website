1a5839c9080862386626012968780f24fc201751
Junaid Malik <junmalik109@gmail.com>
2023-02-23 10:58:22 +0000

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,28 +1,56 @@
 import React from 'react';
 
-import Users from '../compoents/img/banner-img.png';
+// import components
+import Users from '../components/Users';
+
+// import image
+import Img from '../assets/img/banner-img.png';
 
 const Banner = () => {
-
-  return <section>
-    <div className="container mx-auto">
-      <div className="row">
-        <div className="col-md-6">
-          <div className="banner-content">
-            <h1>Get your <span>free</span> account</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien.</p>
-            <a href="#" className="btn btn-primary">Get Started</a>
+  return (
+    <section className='min-h-[600px] pt-24 pb-12 text-center relative lg:pt-48 lg:pb-0 lg:text-left'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+          {/* text */}
+          <div>
+            <h1
+              className='text-3xl font-bold mb-8 lg:text-5xl lg:leading-snug'
+              data-aos='fade-down'
+              data-aos-delay='500'
+            >
+              Let's Explore the <br /> Augmented World Visually.
+            </h1>
+            <p
+              className='font-secondary mb-12 max-w-[440px] mx-auto lg:mx-0'
+              data-aos='fade-down'
+              data-aos-delay='600'
+            >
+              We have a wide range of AR devices that will help you to explore the world visually.
+            </p>
+            {/* btns */}
+            <div
+              className='flex items-center justify-around space-x-4 max-w-[320px] mx-auto mb-12 lg:space-x-8 lg:mx-0 lg:max-w-none lg:justify-start'
+              data-aos='fade-down'
+              data-aos-delay='700'
+            >
+              <button className='btn'>Get it now</button>
+              <a
+                className='border-b-2 border-transparent hover:border-white transition ease-out'
+                href='#'
+              >
+                Explore Device
+              </a>
+            </div>
+            <Users />
           </div>
-        </div>
-        <div className="col-md-6">
-          <div className="banner-img">
-            <img src={Users} alt="banner" />
+          {/* image */}
+          <div data-aos='fade-up' data-aos-delay='800'>
+            <img src={Img} alt='' />
           </div>
         </div>
       </div>
-    </div>
-
-  </section>;
+    </section>
+  );
 };
 
 export default Banner;
