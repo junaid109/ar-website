f4693907909e2bb252bc0159a045f65cb70c4947
Junaid Malik <junmalik109@gmail.com>
2023-02-23 09:36:45 +0000

    Update to Nav compoennt with list of links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className='hidden lg:flex'>
+      <ul className='flex items-center space-x-12 font-secondary'>
+        <li className='nav-link'>
+          <a href='#'>Home</a>
+        </li>
+        <li className='nav-link'>
+          <a href='#'>About</a>
+        </li>
+        <li className='nav-link'>
+          <a href='#'>Services</a>
+        </li>
+        <li className='nav-link'>
+        <a href='#'>Contact</a>
+        </li>
+        <li className='nav-link'>
+          <a href='#'>Blog</a>
+        </li>
+        <btn className='btn'>
+          <a href='#'>Sign up</a>
+        </btn>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
